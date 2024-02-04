71de9f871d8df43947dcc409a40a405685bf926c
JohnMasapantaPozo <john.masapanta@hotmail.com>
2024-02-04 18:52:52 +0100

    Initial commit

---
+++ RestfulDEMO.API/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestfulDEMO.API.Controllers
+{
+
+    // htttps: //localhost:portnumber/api/students
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studens = new string[]
+            {
+                "JOhn", "Juan"
+            };
+
+            return Ok(studens);
+
+        }
+    }
+}
