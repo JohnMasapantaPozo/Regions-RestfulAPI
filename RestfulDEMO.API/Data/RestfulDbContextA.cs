9e3da13ace1bb61d6bb30de900c111bcf2e03865
JohnMasapantaPozo <john.masapanta@hotmail.com>
2024-02-04 22:14:03 +0100

    Add RestfulDbContextA and prepare for EntityFrameworkCore migration

--- RestfulDEMO.API/Data/RestfulDbContextA.cs
+++ RestfulDEMO.API/Data/RestfulDbContextA.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RestfulDEMO.API.Models.Domain;
 
 namespace RestfulDEMO.API.Data
 {
@@ -8,5 +9,14 @@ namespace RestfulDEMO.API.Data
          * Represents a session with the database and provides a set op API's to performa database operations.
          * It funtions as a bridge between the controllers and the database.
          */
+
+        public RestfulDbContextA(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                        
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
     }
 }
--- RestfulDEMO.API/Program.cs
+++ RestfulDEMO.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RestfulDEMO.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/* DB Context class injection:
+    Add connection string to the DB context class.
+    Now the DB Context Class should be available to be used accross all the application from any controler and from any repository.
+*/
+
+builder.Services.AddDbContext<RestfulDbContextA>(
+    (options) => options.UseSqlServer(builder.Configuration.GetConnectionString("RestFulDEMOConnectionString"))
+    );
 
 var app = builder.Build();
 
