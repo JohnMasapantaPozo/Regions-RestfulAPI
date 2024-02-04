22d6d98cd6a76bd8ec8949f7ca44d0d04f687da1
JohnMasapantaPozo <john.masapanta@hotmail.com>
2024-02-04 19:34:58 +0100

    Initialize DB Context Class

---
+++ RestfulDEMO.API/Data/RestfulDbContextA.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestfulDEMO.API.Data
+{
+    public class RestfulDbContextA: DbContext
+    {
+        /* Context Class:
+         * Represents a session with the database and provides a set op API's to performa database operations.
+         * It funtions as a bridge between the controllers and the database.
+         */
+    }
+}
