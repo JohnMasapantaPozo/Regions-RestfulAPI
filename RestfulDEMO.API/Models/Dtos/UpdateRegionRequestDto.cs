b4cc8eec4827edf05c0debf4ee9e833246601f45
JohnMasapantaPozo <john.masapanta@hotmail.com>
2024-02-07 19:42:31 +0100

    Add request and return Dtos fro Regions

---
+++ RestfulDEMO.API/Models/Dtos/UpdateRegionRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestfulDEMO.API.Models.Dtos
+{
+    public class UpdateRegionRequestDto
+    {
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string? RegionImageUrl { get; set; }
+    }
+}
