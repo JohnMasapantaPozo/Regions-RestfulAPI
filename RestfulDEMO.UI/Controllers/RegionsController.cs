ced119e54c7fbe11b89978557bd7af80aefd73a0
JohnMasapantaPozo <john.masapanta@hotmail.com>
2024-03-03 00:46:35 +0100

    Add update funtionality to MVC app

--- RestfulDEMO.UI/Controllers/RegionsController.cs
+++ RestfulDEMO.UI/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using RestfulDEMO.UI.Models;
 using RestfulDEMO.UI.Models.Dto;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 
@@ -10,10 +11,12 @@ namespace RestfulDEMO.UI.Controllers
     public class RegionsController : Controller
     {
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly string baseServer;
 
         public RegionsController(IHttpClientFactory httpClientFactory)
         {
             this.httpClientFactory = httpClientFactory;
+            this.baseServer = "https://localhost:7008";
         }
 
         [HttpGet]
@@ -25,7 +28,7 @@ namespace RestfulDEMO.UI.Controllers
                 // Get all regions from our API
                 var client = httpClientFactory.CreateClient();
 
-                var httpResponseMessagee = await client.GetAsync("https://localhost:7008/api/Regions");
+                var httpResponseMessagee = await client.GetAsync($"{this.baseServer}/api/Regions");
                 httpResponseMessagee.EnsureSuccessStatusCode();
 
                 response.AddRange(await httpResponseMessagee.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
@@ -55,7 +58,7 @@ namespace RestfulDEMO.UI.Controllers
             var httpRequestMessage = new HttpRequestMessage()
             {
                 Method = HttpMethod.Post,
-                RequestUri = new Uri("https://localhost:7008/api/Regions"),
+                RequestUri = new Uri($"{this.baseServer}/api/Regions"),
                 Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
             };
 
@@ -68,9 +71,50 @@ namespace RestfulDEMO.UI.Controllers
             if (response is not null)
             {
                 // Redirect to Index-Regions on success
-                RedirectToAction("Index", "Regions");
+                return RedirectToAction("Index", "Regions");
             }
 
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            // Returns the 'Edit' view to edit the selected region
+
+            var client = httpClientFactory.CreateClient();
+
+            var response = await client.GetFromJsonAsync<RegionDto>($"{this.baseServer}/api/Regions/{id.ToString()}");
+
+            if (response is not null)
+            {
+                return View(response);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDto request)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"{this.baseServer}/api/Regions/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response is not null)
+            {
+                // Redirect to Edit-Regions on success
+                return RedirectToAction("Edit", "Regions");
+            }
             return View();
         }
     }
